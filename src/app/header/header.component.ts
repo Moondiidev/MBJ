1f3edcae24dd28eb2afeb98726d9f0f6a56f2613
MoondiiTech <moondiidev@gmail.com>
2020-07-04 23:50:47 +0800

    app states added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,29 +13,37 @@ import { filter } from 'rxjs/operators';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   currentState: string = "main";
-  isAuthenticated: boolean = false;
+  isBuyerMode: boolean = false;
   isSellerMode: boolean = false;
   profileImgUrl: string = "../../assets/img/profilePlaceholder.svg";
   userName: string;
+  logInMode: string;
+  logInModeName: string = "Худалдагчийн Горим";
+  hasSellerAccount: boolean = false;
   private userNameSub: Subscription;
   private userSub: Subscription;
-  private appStateSub: Subscription;
+  private headerStateSub: Subscription;
   private profileImgSub: Subscription;
+  private logInStateSub: Subscription;
+
   constructor(private authService: AuthService, private appManagerService: AppManagerService, private sellerService: SellerSetUpService, private router: Router) { }
 
   ngOnInit() {
     //If user exists, authenticated
     //I listened to only changes below so, current state needs to be set initially like this
     this.userSub = this.authService.user.subscribe(user => {
-      this.isAuthenticated = !!user;
-      if (this.isAuthenticated) {
-        this.currentState = 'authenticated';
+      this.isBuyerMode = !!user;
+      if (this.isBuyerMode) {
+        this.currentState = this.appManagerService.headerStates.buyerMain;
         this.userNameSub = this.appManagerService.userName.subscribe(name => {
           this.userName = name;
         });
       } else {
-        this.currentState = 'main';
+        this.currentState = this.appManagerService.headerStates.main;
       }
+    })
+    this.logInStateSub = this.appManagerService.logInStateSub.subscribe(state => {
+      this.logInMode = state;
     })
     //Whenever url changes, check it and change the headerState
     this.router.events
@@ -47,13 +55,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
             this.profileImgUrl = url;
           }
         })
-        if (this.isAuthenticated) {
+        if (this.logInMode === 'buyerMode') {
           if (url === '/main') {
-            this.appManagerService.headerStateSub.next(this.appManagerService.headerStates.authenticated);
+            this.appManagerService.headerStateSub.next(this.appManagerService.headerStates.buyerMain);
           } else if (url.includes('/seller-set-up')) {
             this.appManagerService.headerStateSub.next(this.appManagerService.headerStates.onlyLogo);
           }
-        } else if (this.isSellerMode) {
+        } else if (this.logInMode === 'sellerMode') {
           if (url === '/main') {
             this.appManagerService.headerStateSub.next(this.appManagerService.headerStates.sellerMain);
           }
@@ -66,24 +74,44 @@ export class HeaderComponent implements OnInit, OnDestroy {
       )
 
     //Based on the header state, change the header DOM
-    this.appStateSub = this.appManagerService.headerStateSub.subscribe(state => {
+    this.headerStateSub = this.appManagerService.headerStateSub.subscribe(state => {
       if (state != null) {
         this.currentState = state;
         alert(this.currentState);
       }
     });
   }
+  logout() {
+    this.authService.logout();
+    this.appManagerService.switchToVisitorMode();
+  }
+  switchLogInMode() {
+    if (!this.hasSellerAccount) {
+      this.router.navigate(['/seller-set-up/personal']);
+    } else {
+      if (this.isSellerMode) {
+        this.buyerModeActive();
+      } else {
+        this.sellerModeActive();
+      }
+    }
+  }
+  buyerModeActive() {
+    this.isSellerMode = false;
+    this.logInModeName = "Худалдан Авагчийн Горим";
+  }
+  sellerModeActive() {
+    this.isSellerMode = true;
+    this.logInModeName = "Худалдагчийн Горим";
+  }
   onClick() {
     alert("bruh");
   }
   ngOnDestroy() {
     this.userSub.unsubscribe();
-    this.appStateSub.unsubscribe();
+    this.headerStateSub.unsubscribe();
     this.profileImgSub.unsubscribe();
     this.userNameSub.unsubscribe();
+    this.logInStateSub.unsubscribe();
   }
-  logout() {
-    this.authService.logout();
-  }
-
 }
--- src/app/shared/app-manager.service.ts
+++ src/app/shared/app-manager.service.ts
@@ -1,17 +1,42 @@
 import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppManagerService {
+  constructor(private http: HttpClient) { }
+
   headerStateSub = new BehaviorSubject<string>(null);
+  logInStateSub = new BehaviorSubject<string>(null);
+  userName = new BehaviorSubject<string>(null);
+
   headerStates = {
     main: 'main',
-    authenticated: 'authenticated',
+    buyerMain: 'buyerMain',
     sellerMain: 'sellerMain',
     onlyLogo: 'onlyLogo',
   }
-  userName = new BehaviorSubject<string>(null);
-  constructor() { }
+  logInStates = {
+    visitorMode: 'visitorMode',
+    buyerMode: 'buyerMode',
+    sellerMode: 'sellerMode'
+  }
+  switchToVisitorMode(){
+    this.logInStateSub.next(this.logInStates.visitorMode);
+    this.saveLogInState();
+  }
+  switchToBuyerMode(){
+    this.logInStateSub.next(this.logInStates.buyerMode);
+    this.saveLogInState();
+  }
+  switchToSellerMode(){
+    this.logInStateSub.next(this.logInStates.sellerMode);
+    this.saveLogInState();
+  }
+  saveLogInState() {
+    this.http.put(`${environment.cors}${environment.databaseURL}sellerAccounts/${this.userName.value}/logInState.json`, this.logInStateSub.value).subscribe(res => { console.log(res); });
+  }
 }
