13e84055d46464706469110eb6637abfe6a463e1
Munkhbilguun Munkhdemberel <mbprojekt0@gmail.com>
2020-07-17 19:48:52 +0800

    building the seller-board

--- src/app/seller-board/seller-board.component.ts
+++ src/app/seller-board/seller-board.component.ts
@@ -1,4 +1,7 @@
+import { AppManagerService } from './../shared/app-manager.service';
+import { SellerSetUpService } from './../seller-set-up/seller-set-up.service';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-seller-board',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seller-board.component.scss']
 })
 export class SellerBoardComponent implements OnInit {
-
-  constructor() { }
+  profileImgUrl: string = null;
+  personalProfileImgDataSub: Subscription;
+  userNameSub: Subscription;
+  userName: string;
+  responseRate: number = 40;
+  constructor(private sellerService: SellerSetUpService, private appManagerService: AppManagerService) { }
 
   ngOnInit(): void {
+    this.getProfileImage();
+    this.getUserName();
+    
   }
-
+  getProfileImage() {
+    this.personalProfileImgDataSub = this.sellerService.getProfileImg().subscribe(imgUrl=>{
+      this.profileImgUrl = imgUrl;
+    })    
+  }
+  getUserName(){
+    this.userNameSub = this.appManagerService.userName.subscribe(name => {
+      this.userName = name;
+    });
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.personalProfileImgDataSub.unsubscribe();
+  }
 }
