50fe5efe0565917cced282eca0160c7b853e7c03
Munkhbilguun Munkhdemberel <mbprojekt0@gmail.com>
2020-07-27 02:06:18 +0800

    adds dynamic month label

--- src/app/seller-board/seller-board.component.ts
+++ src/app/seller-board/seller-board.component.ts
@@ -19,6 +19,8 @@ export class SellerBoardComponent implements OnInit {
   today = new Date();
   months: Array<string> = ['Нэгдүгээр Сар', 'Хоёрдугаар Сар', 'Гуравдугаар Сар', 'Дөрөвдүгээр Сар', 'Тавдугаар Сар', 'Зургадугаар Сар', 'Долдугаар Сар', 'Наймдугаар Сар', 'Есдүгээр Сар', 'Аравдугаар Сар', 'Араваннэгдүгээр Сар', 'Араванхоёрдугаар Сар'];
   monthIndex: number = this.today.getMonth();
+  monthLabelIndexes: Array<number> = [];
+  chartLabels = [];
   messagePreviews = [
     { profileImg: '../../assets/img/photo.svg', name: 'allaab', message: 'Yu bn haraal idsen ass...', date: '7 cap' },
     { profileImg: '../../assets/img/photo.svg', name: 'allaab', message: 'Yu bn haraal idsen ass...', date: '7 cap' },
@@ -29,11 +31,11 @@ export class SellerBoardComponent implements OnInit {
   constructor(private sellerService: SellerSetUpService, private appManagerService: AppManagerService) { }
 
   barChartData: ChartDataSets[] = [
-    { data: [20590, 100300, 100232, 100232, 100232], label: 'Орлого ₮-өөр' },
+    { data: [20590, 100300, 100232, 100232, 100232, 20590, 100300, 100232, 100232, 100232, 20590, 100300, 100232, 100232, 100232, 20590, 100300, 100232, 100232, 100232, 20590, 100300, 100232, 100232, 100232, 20590, 100300, 100232, 100232, 100442], label: 'Орлого ₮-өөр' },
     { data: [12590, 12300], label: 'Цуцлагдсан ₮-өөр' },
   ];
 
-  barChartLabels: Label[] = [];
+  barChartLabels: Label[] = this.chartLabels;
 
   barChartOptions = {
     responsive: true,
@@ -69,7 +71,24 @@ export class SellerBoardComponent implements OnInit {
   barChartPlugins = [];
   barChartType = 'bar';
 
-  determineLabelIndex() {
+  addMonthLabels() {
+    //Combine current month with neighbour months and then sort them 
+    let labelMonth: Array<number> = [this.monthIndex, ...this.getNeighbourMonths()];
+    labelMonth.sort();
+    this.monthLabelIndexes.forEach((index, i) => {
+      this.labelEmptySpace(index);
+      //Label month name in between empty labels
+      this.chartLabels.push(this.months[labelMonth[i]]);
+      // -1 to compensate the month label name. Without it, there will be 32 divisions instead of 30
+      this.labelEmptySpace(index-1);
+    });
+  }
+  labelEmptySpace(index){
+    for (let i = 0; i < index; i++) {
+      this.chartLabels.push('');
+    }
+  }
+  getNeighbourMonths() {
     // Need 30 days and have respective month in the middle of days that are being used as a label. 
     const todayDayNumber = this.today.getDate();
     const daysInThisMonth = this.daysInMonth(this.today.getMonth());
@@ -80,44 +99,48 @@ export class SellerBoardComponent implements OnInit {
     This adds up to usable 29 days which is less than 30 so, March comes to save the day. 
     So, our graph ends up using Jan, Feb and March.
     */
-    let monthLabelIndexes: Array<number> = [];
     let usableLabelDays: number = 0;
-    let daysInNeighbourMonth: number = 0;
-    let neededDaysInNeighbourMonth: number = 0;
-    let daysInSecondNeighbourMonth: number = 0;
-    let neededDaysInSecondNeighbourMonth: number = 0;
+    let neighbourMonths = [];
+    let daysInNeighbourMonths: Array<number> = [];
+    let neededDaysInNeighbourMonths: Array<number> = [];
     thisMonthLeftOver = daysInThisMonth - todayDayNumber;
 
     // See if you need another month space or not and find the center of needed 30 days to insert those months.
     if (todayDayNumber < daysInThisMonth / 2) {
-      daysInNeighbourMonth = this.daysInMonth(this.today.getMonth() - 1);
-      usableLabelDays = thisMonthLeftOver + daysInNeighbourMonth;
+      neighbourMonths[0] = this.today.getMonth() - 1;
+      daysInNeighbourMonths[0] = this.daysInMonth(neighbourMonths[0]);
+      usableLabelDays = thisMonthLeftOver + daysInNeighbourMonths[0];
 
       if (usableLabelDays <= 30) {
-        daysInSecondNeighbourMonth = this.today.getMonth() - 2;
+        neighbourMonths[1] = this.today.getMonth() - 2;
+        daysInNeighbourMonths[1] = this.daysInMonth(neighbourMonths[1]);
       }
     } else {
-      daysInNeighbourMonth = this.daysInMonth(this.today.getMonth() + 1);
-      usableLabelDays = thisMonthLeftOver + daysInNeighbourMonth;
+      neighbourMonths[0] = this.today.getMonth() + 1;
+      daysInNeighbourMonths[0] = this.daysInMonth(neighbourMonths[0]);
+      usableLabelDays = thisMonthLeftOver + daysInNeighbourMonths[0];
 
       if (usableLabelDays <= 30) {
-        daysInSecondNeighbourMonth = this.today.getMonth() + 2;
-        neededDaysInSecondNeighbourMonth = daysInSecondNeighbourMonth - thisMonthLeftOver - daysInNeighbourMonth;
+        neighbourMonths[1] = this.today.getMonth() + 2;
+        daysInNeighbourMonths[1] = this.daysInMonth(neighbourMonths[1]);
+        neededDaysInNeighbourMonths[1] = daysInNeighbourMonths[1] - thisMonthLeftOver - daysInNeighbourMonths[0];
       } else {
-        neededDaysInNeighbourMonth = daysInNeighbourMonth - thisMonthLeftOver;
+        neededDaysInNeighbourMonths[0] = daysInNeighbourMonths[0] - thisMonthLeftOver;
       }
     }
-    monthLabelIndexes.push(Math.floor(thisMonthLeftOver / 2));
-
+    //Determine Label Index
+    this.monthLabelIndexes[0] = (Math.floor(thisMonthLeftOver / 2));
     // push only if they are able to be inserted
-    if (neededDaysInNeighbourMonth !== 0) {
-      monthLabelIndexes.push(Math.floor(neededDaysInNeighbourMonth / 2));
+    if (neededDaysInNeighbourMonths[0] != 0 && neededDaysInNeighbourMonths[0] != undefined) {
+      this.monthLabelIndexes[1] = (Math.floor(neededDaysInNeighbourMonths[0] / 2));
     }
-    if (neededDaysInSecondNeighbourMonth !== 0) {
-      monthLabelIndexes.push(Math.floor(neededDaysInSecondNeighbourMonth / 2));
+    if (neededDaysInNeighbourMonths[1] != 0 && neededDaysInNeighbourMonths[1] != undefined) {
+      this.monthLabelIndexes[2] = (Math.floor(neededDaysInNeighbourMonths[1] / 2));
     }
-    console.log(monthLabelIndexes);
-    return monthLabelIndexes;
+
+    console.log(this.monthLabelIndexes);
+    console.log(neighbourMonths);
+    return neighbourMonths;
   }
   daysInMonth(month) {
     return new Date(this.today.getFullYear(), month, 0).getDate();
@@ -125,7 +148,7 @@ export class SellerBoardComponent implements OnInit {
   ngOnInit(): void {
     this.getProfileImage();
     this.getUserName();
-    this.determineLabelIndex();
+    this.addMonthLabels();
   }
   getProfileImage() {
     this.personalProfileImgDataSub = this.sellerService.getProfileImg().subscribe(imgUrl => {
