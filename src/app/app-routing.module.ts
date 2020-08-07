35b6e3c054bb76fbbb05140f12ae06fd18a9a55b
Munkhbilguun Munkhdemberel <mbprojekt0@gmail.com>
2020-08-08 00:47:22 +0800

    sellersetup btn style changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/main', pathMatch: 'full'},
-  {path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule)},
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-  {path: 'seller-set-up', loadChildren: () => import('./seller-set-up/seller-set-up.module').then(m => m.SellerModule)},
-  {path: 'seller-board', loadChildren: () => import('./seller-board/seller-board.module').then(m => m.SellerBoardModule)},
-  {path: 'user-profile', loadChildren: () => import('./user-profile/user-profile.module').then(m => m.UserProfileModule)},
-  {path: 'gig-creation', loadChildren: () => import('./gig-creation/gig-creation.module').then(m => m.GigCreationModule)}
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'seller-set-up', loadChildren: () => import('./seller-set-up/seller-set-up.module').then(m => m.SellerModule) },
+  { path: 'seller-board', loadChildren: () => import('./seller-board/seller-board.module').then(m => m.SellerBoardModule) },
+  { path: 'user-profile', loadChildren: () => import('./user-profile/user-profile.module').then(m => m.UserProfileModule) },
+  { path: 'gig-creation', loadChildren: () => import('./gig-creation/gig-creation.module').then(m => m.GigCreationModule) }
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes,{preloadingStrategy: PreloadAllModules})],
+  imports: [RouterModule.forRoot(appRoutes, {
+    preloadingStrategy: PreloadAllModules,
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
